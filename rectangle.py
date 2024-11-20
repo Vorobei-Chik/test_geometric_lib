ba7f4781e488e93daef6db6d386d279b19dc5f04
Медведев Павел 466677 <pk.medvedev@mail.ru>
2024-11-20 20:29:12 +0300

    Added rectangle tests

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,36 @@
+import unittest
+
+
 def area(a, b):
     '''Принимает два десятичных числа a и b, возвращает площадь прямоугольника со сторонами a и b'''
     return a * b
 
+
 def perimeter(a, b):
     '''Принимает два десятичных числа a и b, возвращает периметр прямоугольника со сторонами a и b'''
     return (a + b) * 2
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        self.assertEqual(area(0, 0), 0)
+    def test_one_area(self):
+        self.assertEqual(area(1, 1), 1)
+    def test_float_area(self):
+        self.assertAlmostEqual(area(1.2, 3.4), 4.08)
+    def test_square_area(self):
+        self.assertEqual(area(8, 8), 64)
+    def test_random_area(self):
+        self.assertEqual(area(123, 654), 80442)
+
+    def test_zero_perimeter(self):
+        self.assertEqual(perimeter(0, 0), 0)
+    def test_one_perimeter(self):
+        self.assertEqual(perimeter(1, 1), 4)
+    def test_float_perimeter(self):
+        self.assertAlmostEqual(perimeter(123.654, 456.987), 1161.282)
+    def test_square_perimeter(self):
+        self.assertEqual(perimeter(8, 8), 32)
+    def test_random_perimeter(self):
+        self.assertEqual(perimeter(123654, 456987), 1161282)
+
