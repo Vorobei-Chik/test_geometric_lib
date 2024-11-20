3b27acf927ef1c600fb22b32a327368332e8880d
Медведев Павел 466677 <pk.medvedev@mail.ru>
2024-11-20 20:08:06 +0300

    Added circle tests

--- circle.py
+++ circle.py
@@ -1,4 +1,5 @@
 import math
+import unittest
 
 
 def area(r):
@@ -9,4 +10,24 @@ def area(r):
 def perimeter(r):
     '''Принимает десятичное число r, возвращает периметр круга радиуса r'''
     return 2 * math.pi * r
+
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        self.assertEqual(area(0), 0)
+    def test_one_area(self):
+        self.assertEqual(area(1), math.pi)
+    def test_float_area(self):
+        self.assertEqual(area(1.2), 1.44 * math.pi)
+    def test_random_area(self):
+        self.assertEqual(area(123654), 15290311716 * math.pi)
+
+    def test_zero_perimeter(self):
+        self.assertEqual(perimeter(0), 0)
+    def test_one_perimeter(self):
+        self.assertEqual(perimeter(1), 2 * math.pi)
+    def test_float_perimeter(self):
+        self.assertEqual(perimeter(1.2), 2.4 * math.pi)
+    def test_random_perimeter(self):
+        self.assertEqual(perimeter(123654), 247308 * math.pi)
 
